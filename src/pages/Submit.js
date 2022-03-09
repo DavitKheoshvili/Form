218fb16de97eedc3699bd78065923e54ff557f61
Davit <kheoshvili.dato@gmail.com>
2022-03-09 17:19:13 +0400

    fixed some errors.

--- src/pages/Submit.js
+++ src/pages/Submit.js
@@ -2,10 +2,19 @@ import React, {useState} from "react"
 import axios from "axios"
 
 function Submit({page, setPage, result, setResult}) {
-  
-    
-  
+    console.log(result, typeof result.phone, typeof result.devtalk_topic)
+    if(result.phone == "") {
+      setResult({...result, phone: "+995512123456"})
+    }
+    if(result.devtalk_topic == "") {
+      setResult({...result, devtalk_topic: "The user left this box empty"})
+    }
+    console.log("checked",result, typeof result.phone, typeof result.devtalk_topic)
+
   const submitClicked = (e) => {
+    
+    console.log("clicked", result, typeof result.phone, typeof result.devtalk_topic)
+
     e.preventDefault();
     axios.post("https://bootcamp-2022.devtest.ge/api/application", result)
     setPage(page + 1)
